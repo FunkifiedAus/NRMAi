4964d83273ac35cb545ef9d6aff7ae963883abdc
FunkifiedAus <jacob.masters@vinyl.media>
2026-06-04 11:48:36 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    - Passes through Apps Script + Drive requests without touching them,
      so passcode / catalogue / order calls always hit live data.
 */
-const VERSION = 'nrmai-portal-v27-2026-05-25-open-edit-access';
+const VERSION = 'nrmai-portal-v28-2026-05-25-cloudflare-proxy';
 const APP_SHELL = [
   './',
   './index.html',
